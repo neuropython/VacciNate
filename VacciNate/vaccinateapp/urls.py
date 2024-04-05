bbbce887f616b8177587afafb150346267efe832
Damian <damianpietron007@gmail.com>
2024-04-05 21:47:36 +0200

    Integrare urls with VaccinateDetail view

--- VacciNate/vaccinateapp/urls.py
+++ VacciNate/vaccinateapp/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.urls import include
 from .api.views import (VaccinateAll,
-                        UserVaccinateAll)
+                        UserVaccinateAll,
+                        VaccinateDetail)
 
 urlpatterns = [
     path('list/', VaccinateAll.as_view(), name='vaccination-list'),
+    path('detail/<int:pk>/', VaccinateDetail.as_view(), name='vaccination-detail'),
     path('user/', UserVaccinateAll.as_view(), name='user-vaccination-list')
 ]
