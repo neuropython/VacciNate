99d6ffbc0d846fb9cdc57ccd71cc0c45f7248474
Damian <damianpietron007@gmail.com>
2024-04-06 17:24:34 +0200

    Change settings to be more suitable for quicker development

--- VacciNate/VacciNate/settings.py
+++ VacciNate/VacciNate/settings.py
@@ -170,7 +170,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     'ROTAATE_REFRESH_TOKENS': True,
 }
