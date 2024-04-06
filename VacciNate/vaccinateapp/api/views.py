2249af7bc87952bdbbc7fbaca210b1c5281805c9
Damian <damianpietron007@gmail.com>
2024-04-06 13:44:58 +0200

    Fix UserVaccinateAll view

--- VacciNate/vaccinateapp/api/views.py
+++ VacciNate/vaccinateapp/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers, generics
-from vaccinateapp.models import Vaccine, User
+from vaccinateapp.models import Vaccine, User, UserVaccine
 from .serializer import VaccinateSerializer, UserVaccineSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -13,9 +13,13 @@ class VaccinateDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Vaccine.objects.all()
     serializer_class = VaccinateSerializer
     
-class UserVaccinateAll(generics.ListCreateAPIView):
+class UserVaccinateAll(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
     queryset = User.objects.all()
     serializer_class = UserVaccineSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return  UserVaccine.objects.filter(user__username=user)
