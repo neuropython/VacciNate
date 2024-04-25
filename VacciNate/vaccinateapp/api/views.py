1f2a98ddc34e30d8a9644570ef8aef249c0b7e83
Damian <damianpietron007@gmail.com>
2024-04-25 08:33:16 +0200

    add docs

--- VacciNate/vaccinateapp/api/views.py
+++ VacciNate/vaccinateapp/api/views.py
@@ -6,14 +6,39 @@ from rest_framework.permissions import IsAuthenticated
 
 
 class VaccinateAll(generics.ListCreateAPIView):
+    """
+    This class is used to get all the vaccines and add a new vaccine
+    
+    get: Get all the vaccines   
+    post: Add a new vaccine
+    """
     queryset = Vaccine.objects.all()
     serializer_class = VaccinateSerializer
 
 class VaccinateDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    This class is used to get, update and delete a vaccine
+    
+    get: Get a vaccine
+    put: Update a vaccine
+    delete: Delete a vaccine
+    patch: Partial update of a vaccine
+    head: check if a vaccine exists
+    options: Get the allowed methods
+    """
     queryset = Vaccine.objects.all()
     serializer_class = VaccinateSerializer
     
 class UserVaccinateAll(generics.RetrieveUpdateDestroyAPIView):
+    """
+    This class is used to get all the user vaccines
+    
+    get: Get all the user vaccines
+    put: Update a user vaccine
+    delete: Delete a user vaccine
+    patch: Partial update of a user vaccine
+    options: Get the allowed methods
+    """
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
