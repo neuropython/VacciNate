868322c116220c3d4ba7e840b60e3d3d83ed4ec7
Damian <damianpietron007@gmail.com>
2024-05-07 12:18:47 +0200

    chore: Set logging level to INFO in celery.py

--- VacciNate/VacciNate/celery.py
+++ VacciNate/VacciNate/celery.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from celery import Celery
 from django.conf import settings
@@ -8,13 +9,20 @@ env = environ.Env()
 environ.Env.read_env()
 REDIS_URL = env("REDIS_URL")
 
+logging.basicConfig(level=logging.INFO)  # Set the logging level to INFO
+logger = logging.getLogger(__name__)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VacciNate.settings')
-app = Celery('VacciNate', broker=REDIS_URL, backend=REDIS_URL, broker_heartbeat=0)
+app = Celery('VacciNate', broker=REDIS_URL, backend=REDIS_URL, broker_heartbeat=2)
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.conf.broker_connection_retry_on_startup = True
 
+def on_redis_connect(sender=None, **kwargs):
+    logger.info("Connected to Redis broker!")
+
+app.on_configure.connect(on_redis_connect)  # Register the callback function
 app.autodiscover_tasks()
 
 @app.task(bind=True, ignore_result=True)
