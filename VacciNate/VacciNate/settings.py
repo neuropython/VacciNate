e3678d2cc606cc2570c50e3d685e4a0bca8e4f28
Damian <damianpietron007@gmail.com>
2024-05-07 22:06:22 +0200

    chore: Update user throttle rate to 300/day in settings.py

--- VacciNate/VacciNate/settings.py
+++ VacciNate/VacciNate/settings.py
@@ -182,7 +182,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '100/day',
-        'user': '200/day',
+        'user': '300/day',
         'review-create-throttle': '7/day'
     },
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
